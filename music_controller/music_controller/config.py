717df88faff1226091958da9e98dc9d0ff00a7ec
Misha <moborin4@gmail.com>
2023-11-24 17:38:22 +0200

    add RoomView

---
+++ music_controller/music_controller/config.py
@@ -0,0 +1,7 @@
+db_pass = "IIgbyyl3ACH0ABzp"
+
+db_name = "colaborative_music_playing_system_django-react"
+
+db_url = "mongodb+srv://moborin4:{password}@cluster0.hbtuded.mongodb.net/?retryWrites=true&w=majority".format(
+    password=db_pass
+)
